7c5538861814b9f679a3ecb2d8cfd6d4e79cc272
Dennis Blaesing <blaesing@cosmocode.de>
2012-11-10 23:12:14 +0100

    introduced helper class

---
+++ AS_Projekt/helper/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using as_projekt.data;
+
+namespace AS_Projekt.helper
+{
+    public class Helper
+    {
+        public static Employee CreateEmployee(int id, String firstname, String lastname, int gender, int departmend_id)
+        {
+            return new Employee(id, firstname, lastname, (EmployeeGender)gender, new Department("bla"));
+        }
+    }
+}
