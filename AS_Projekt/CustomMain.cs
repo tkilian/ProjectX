ff67782a64fddc5be72a4afa2d9f243f40c41f32
Stephan Weikelt <s.weikelt@hotmail.de>
2012-11-14 15:46:37 +0100

    ShellOutput / CustomMain/ConsoleManager

---
+++ AS_Projekt/CustomMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AS_Projekt
+{
+  class CustomMain
+  {
+    /// <summary>
+    /// Application Entry Point.
+    /// </summary>
+    [System.STAThreadAttribute()]
+    [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    public static void Main()
+    {
+      //DB oder XML
+
+
+      //Startet Konsolenanwendung
+      RunConsole();
+      //Startet WPF-Dreck
+      //RunWPF();
+         
+    }
+
+    private static void RunConsole()
+    {
+      ConsoleManager.ConsoleManage.Show();
+      Console.WriteLine("HALLO");
+      System.Diagnostics.Trace.WriteLine("HALLO");
+      Console.ReadLine();   
+    }
+
+    private static void RunWPF()
+    {
+      AS_Projekt.App app = new AS_Projekt.App();
+      app.InitializeComponent();
+      app.Run();
+    }
+  }
+}
---
+++ AS_Projekt/ShellOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AS_Projekt
+{
+  class ShellOutput
+  {
+    public ShellOutput()
+    {
+    }
+  }
+}
