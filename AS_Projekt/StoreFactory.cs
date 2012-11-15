abd4cf233d26806659c643ada67f231196afa991
Domenico Iavicoli <domenico-iavicoli@web.de>
2012-11-15 18:27:03 +0100

    kleiner fix

--- AS_Projekt/StoreFactory.cs
+++ AS_Projekt/StoreFactory.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using AS_Projekt.interfaces;
 using AS_Projekt.db;
+using AS_Projekt.interfaces;
 using AS_Projekt.xml;
 
 namespace AS_Projekt
